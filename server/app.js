122d660af090c72e336674ec5bcd535abc552b06
Maicom <maicom.azus@gmail.com>
2024-10-14 13:51:52 -0300

    Testando com novos

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const app = express();
 const routes = require('./src/routes');
 const prisma = require('./src/utils/dbConnect');
 const path = require('path');
+
 app.use(express.json());
 app.use(cors()); // Permite todas as origens
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
