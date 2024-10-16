99c9504de75a67cc2bb11f0c623846f27fa03fb6
Maicom <maicom.azus@gmail.com>
2024-10-16 14:43:11 -0300

    comentando as rotas cors header

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(cors({
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://app.fluxodocapital.com.br'); // Substitua pela sua origem
+    res.header('Access-Control-Allow-Origin', 'https://*.fluxodocapital.com.br');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
