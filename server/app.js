f5e4bf0770a26aa47a35f6c500d0b817df5457ed
Maicom <maicom.azus@gmail.com>
2024-10-16 13:45:27 -0300

    Ajuste cors telegram

--- server/app.js
+++ server/app.js
@@ -7,23 +7,12 @@ const prisma = require('./src/utils/dbConnect');
 const path = require('path');
 
 app.use(express.json());
+
 app.use(cors({
-    origin: '*',
+    origin: 'https://app.fluxodocapital.com.br'
 }));
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://app.fluxodocapital.com.br'); // Defina a origem permitida
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Métodos HTTP permitidos
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Cabeçalhos permitidos
-    res.setHeader('Access-Control-Allow-Credentials', 'true'); // Permitir envio de cookies
 
-    // Se for uma requisição OPTIONS (preflight), responde com status 200
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    }
-
-    next();
-});
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/', routes);
