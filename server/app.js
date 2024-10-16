4c456dd8688bdf2972b3ba6c4004002c82d8f44d
Maicom <maicom.azus@gmail.com>
2024-10-15 21:59:42 -0300

    Ajustando o cors

--- server/app.js
+++ server/app.js
@@ -8,14 +8,28 @@ const path = require('path');
 
 app.use(express.json());
 app.use(cors({
-    origin: '*'
+    origin: '*',
 }));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://app.fluxodocapital.com.br'); // Defina a origem permitida
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Métodos HTTP permitidos
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Cabeçalhos permitidos
+    res.setHeader('Access-Control-Allow-Credentials', 'true'); // Permitir envio de cookies
+
+    // Se for uma requisição OPTIONS (preflight), responde com status 200
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+
+    next();
+});
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/', routes);
-const port = 3306;
+const port = process.env.PORT || 3306; // Escolher uma porta apropriada
 app.listen(port, () => {
-    console.log("Servidor rodando na porta", port);
+    console.log(`Servidor rodando na porta ${port}`);
 });
 
 
