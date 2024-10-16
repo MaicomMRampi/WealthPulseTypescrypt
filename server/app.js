b59d7e88fcf1d3765c49641f93db4cbbad640caa
Maicom <maicom.azus@gmail.com>
2024-10-16 18:24:59 -0300

    Ajuste de corsss

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+const dotenv = require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -6,21 +6,15 @@ const routes = require('./src/routes');
 const prisma = require('./src/utils/dbConnect');
 const path = require('path');
 
-app.use(express.json());
 
-app.use(cors({
-    origin: 'https://app.fluxodocapital.com.br/pages/register'
-}));
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://*.fluxodocapital.com.br');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-    next();
-});
 app.use('/', routes);
 const port = process.env.PORT || 3306; // Escolher uma porta apropriada
 app.listen(port, () => {
