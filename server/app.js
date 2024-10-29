5defcb497f2984acbf5d6d972d9581119ba23f4c
Maicom <maicom.azus@gmail.com>
2024-10-29 11:49:59 -0300

    novo server com rotas

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 3333
+const router = require('./routes/userRoutes')
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
 
-module.exports = app;
+app.use(router)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+module.exports = app
